feccee2d310f992c13840fbee1cecef483579619
Arturo1s <arturodelpatio1@gmail.com>
2025-01-13 15:59:14 +0100

    Add existing file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Todolist}',
+ version='1.0',
+ author='{Arturo1s}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
